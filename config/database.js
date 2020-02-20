e0e1c46bc6efedeef8c0be46711242b5eb4eece0
Guillaume97490 <techer.guillaume97490@gmail.com>
2020-02-20 11:28:54 +0400

    add procfile

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 var ObjectId = require('mongodb').ObjectID;
 
-mongoose.connect('mongodb://localhost:27017/qrcode-gen',{
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/qrcode-gen',{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false 
