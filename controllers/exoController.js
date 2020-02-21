ff8b0886c8eedd7d30ce7e580b1b139c77e016c5
Guillaume97490 <techer.guillaume97490@gmail.com>
2020-02-21 13:51:22 +0400

    add parley validate && recaptcha

--- controllers/exoController.js
+++ controllers/exoController.js
@@ -3,11 +3,12 @@ let Exo = require('../models/exo');
 const validator = require('validator');
 const paginate = require('express-paginate');
 const QRCode = require('qrcode');
+const fetch = require('isomorphic-fetch');
 
 
 controller.index = async (req, res, next) => {
     try {
-        const [ results, itemCount ] = await Promise.all([
+        const [results, itemCount] = await Promise.all([
             Exo.find({}).limit(req.query.limit).skip(req.skip).lean().exec(),
             Exo.countDocuments({})
         ]);
@@ -26,43 +27,80 @@ controller.index = async (req, res, next) => {
 }
 
 
-controller.save = (req, res) => {
-    try {
-        var url = req.body.url_input.replace(/^(https?):\/\//gm, '');
+controller.save = async (req, res) => {
+    // try {
+    // console.log(req.body);
+    const secret_key = process.env.SECRET_KEY;
+    const token = req.body.token;
+    const verifUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token}`;
 
-        let newUrl = Exo({
-            url:  validator.escape(url),
-        })
-        newUrl.nom = validator.escape('item-' + newUrl._id).trim()
-        newUrl.save(function(err) {
-        if (err) throw err;
-        res.redirect("/"); 
-        });
-    } catch (error) {
-        
+
+    if (!process.env.SECRET_KEY){
+        if (req.body.url_input == "") return res.redirect("/");
+            var url = req.body.url_input.replace(/^(https?):\/\//gm, '');
+
+            let newUrl = Exo({
+                url: validator.escape(url),
+            });
+            newUrl.nom = validator.escape('item-' + newUrl._id).trim();
+            newUrl.save(function (err) {
+                if (err) throw err;
+                res.redirect("/");
+            });
     }
-    
+
+    const response = await fetch(verifUrl, {
+        method: 'post'
+    })
+        .then(response => response.json())
+        .then(google_response => {
+            if (google_response.success == true) {
+                // todo; save et redirrect
+                // res.json("true")
+
+                if (req.body.url_input == "") return res.redirect("/");
+                var url = req.body.url_input.replace(/^(https?):\/\//gm, '');
+
+                let newUrl = Exo({
+                    url: validator.escape(url),
+                });
+                newUrl.nom = validator.escape('item-' + newUrl._id).trim();
+                newUrl.save(function (err) {
+                    if (err) throw err;
+                    res.redirect("/");
+                });
+            }
+            // res.json({ google_response })
+        })
+        .catch(error => res.json({ error }));
+
+    // console.log(response);
+
+    // } catch (error) {
+
+    // }
+
 }
 
 controller.item = (req, res) => {
     try {
-        Exo.findOne({nom:req.params.id},(err,url)=>{
+        Exo.findOne({ nom: req.params.id }, (err, url) => {
             res.redirect('https://' + url.url);
         });
     } catch (error) {
-        
+
     }
-    
+
 }
 
-controller.qrcode = (req,res) => {
+controller.qrcode = (req, res) => {
     try {
-        Exo.findOne({nom:req.params.item},(err,url)=>{
+        Exo.findOne({ nom: req.params.item }, (err, url) => {
             QRCode.toDataURL(`https://qr-code-generator-2020.herokuapp.com/item/${url.nom}`, function (err, url) {
                 res.json(url);
-              })
+            })
         }).lean();
-    } catch (error) {   
+    } catch (error) {
     }
 }
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,37 @@
 
 $(() => {
 
-    $(document).on('click', '[data-id]', function(e){
-        e.preventDefault()
-        let id = $(this).data('id')
-        // console.log(id)
-        $.get(`/qrcode/${id}`, (data) => {
-            let image = new Image();
-            image.src = data;
-            $('#qrcode').append(image);
-            // $('$qrcode img').css('width','50%')
-            $('#myModal').modal('toggle');
-        });
+  $('#submit-btn').click((e) => {
+    e.preventDefault();
+    grecaptcha.ready(function () {
+      grecaptcha.execute('6Lep6NoUAAAAABVg-Rt15HlXBohuCJK7QhggzQgT', { action: 'homepage' }).then(function (token) {
+        $('#form-url').prepend('<input type="hidden" name="token" value="' + token + '">');
+        $('#form-url').submit();
       });
+    });
 
-      $('#myModal').on('hidden.bs.modal', function (e) {
-        $('#qrcode').html('')
-      })
+  })
+
+  $('#url-input').on('keyup', e => {
+    $('#submit-btn').addClass('disabled');
+    if ($('#form-url').parsley().validate()) {
+      $('#submit-btn').removeClass('disabled');
+    }
+
+  })
+
+  $(document).on('click', '[data-id]', function (e) {
+    e.preventDefault();
+    let id = $(this).data('id');
+    $.get(`/qrcode/${id}`, (data) => {
+      let image = new Image();
+      image.src = data;
+      $('#qrcode').append(image);
+      $('#myModal').modal('toggle');
+    });
+  });
+
+  $('#myModal').on('hidden.bs.modal', function (e) {
+    $('#qrcode').html('');
+  })
 })
