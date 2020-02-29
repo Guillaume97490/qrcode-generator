9ec46962a5de94e7372ec759358fba9559a78054
Guillaume97490 <techer.guillaume97490@gmail.com>
2020-02-29 19:13:12 +0400

    fix: qrcode on list

--- views/templates/lastUrlList.js
+++ views/templates/lastUrlList.js
@@ -8,7 +8,7 @@ exports.data = `
                         <div class="col-lg-12 d-flex align-items-center">
                             <div class="row col-12 d-flex align-items-center">
                                 <div class="col-10">
-                                    <a class="text-white text-break" data-id="<%= url._id%>" href="" target="_blank" rel="noopener noreferrer"><%= data. siteUrl%>url/<%= url._id%></a>
+                                    <a class="text-white text-break" data-qr-id="<%= url._id%>" href="" target="_blank" rel="noopener noreferrer"><%= data. siteUrl%>url/<%= url._id%></a>
                                 </div>
                                 <div class="col-2 text-right">
                                     <button data-copy-id="<%= url._id%>" type="button" class="btn" data-toggle="tooltip" data-placement="top" title="Copier le lien">
